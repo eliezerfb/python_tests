1d44b834df2144aca2f112cc456d654f5c4bc759
eliezerfb <eliezerfb@gmail.com>
2018-11-08 16:08:16 -0200

    add info

--- extract_data_from_xml/import_nfe.py
+++ extract_data_from_xml/import_nfe.py
@@ -1,3 +1,15 @@
+""""
+Importação dos dados do XML da NF-e para uma tabela em banco de dados já
+existente. Os dados importados serão utilizados para a geração de CT-e
+(Conhecimento de Frete Eletrônico) e vinculados a um Manifesto de
+Documento Fiscal Eletrônico (MDF-e).
+
+Para executar é necessário passar como parâmetro o ID do manifesto. Exemplo:
+
+python import_nfe.py 34
+
+""""
+
 import glob
 import os
 import sys

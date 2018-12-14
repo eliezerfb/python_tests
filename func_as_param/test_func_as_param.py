940bfa58e13c9b4c440b0f2da99673b9aa20329c
eliezerfb <eliezerfb@gmail.com>
2018-12-14 11:34:52 -0200

    change teste_convert to converter

--- func_as_param/test_func_as_param.py
+++ func_as_param/test_func_as_param.py
@@ -4,19 +4,19 @@ How run: python -m unittest
 import unittest
 
 
-def teste_convert(value, cast):
+def converter(value, cast):
     return cast(value)
 
 
 class TestCast(unittest.TestCase):
     def test_int(self):
-        conv = teste_convert('1', lambda x: int(x))
+        conv = converter('1', lambda x: int(x))
         self.assertEqual(conv, 1)
 
     def test_float(self):
-        conv = teste_convert('1', lambda x: float(x))
+        conv = converter('1', lambda x: float(x))
         self.assertEqual(conv, 1.0)
 
     def test_str(self):
-        conv = teste_convert(1, lambda x: str(x))
+        conv = converter(1, lambda x: str(x))
         self.assertEqual(conv, '1')
